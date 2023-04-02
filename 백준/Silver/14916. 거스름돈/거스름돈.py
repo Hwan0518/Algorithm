be5652b95add0ae53d0e4d735696f1ef02cc7181
Hwan0518 <108791919+Hwan0518@users.noreply.github.com>
2023-04-02 22:10:07 +0900

    [Silver V] Title: 거스름돈, Time: 44 ms, Memory: 32820 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.py
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -29,14 +29,21 @@ Access
 """수학풀이"""
 #define function
 def solution():
-    quotient = (n//5)
-    remainder = n-(quotient*5)
-    while remainder==1 or remainder ==3:
-        quotient -=1
-        remainder = n-(quotient*5)
-    result = quotient + remainder//2
+    # 5의 배수인지 확인
+    if n%5 ==0:
+        return n//5
     
-    return result
+    # 5의 배수가 아니라면, (5로 나눈 나머지)가 2의 배수인지 확인
+    remainder = n%5
+    if remainder %2 ==0:
+        return (n//5) + (remainder//2)
+    
+    # 나머지가 2의 배수가 아니라면, (5로 나눈 나머지+5)가 2의 배수인지 확인
+    if (remainder+5)%2 ==0 and n>5:
+        return (n//5-1) + (remainder+5)//2
+    
+    # 아니라면 -1출력
+    return -1    
 
 
 """dp풀이 : bottom-up"""
@@ -59,7 +66,21 @@ def bottomUp():
 #main
 n=int(input())
 dp = [0,-1,1,-1,2,1] + [0 for _ in range(n+1)]
-# print(solution())
-print(bottomUp())
+print(solution())
+# print(bottomUp())
+
+"""
+반례)
+1:-1
+2:1
+3:-1
+4:2
+5:1
+6:3
+7:2
+8:4
+9:3
+10:2
+"""
 
 
