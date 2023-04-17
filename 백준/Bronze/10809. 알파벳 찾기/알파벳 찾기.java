f37ee11d9b3792e5b0a089ea6cbef907daccfc32
Hwan0518 <108791919+Hwan0518@users.noreply.github.com>
2023-04-17 20:43:24 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 148 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,45 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
+/*
+Condition
+    - TL :
+      ML :
+
+Question
+    -
+
+Access
+    -
+
+input&output hint
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+StringBuffer sb = new StringBuffer();
+String input = br.readLine();
+int val = Integer.parseInt(input);
+sout(sb);
+br.close();
+*/
+
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuffer sb = new StringBuffer();
 
-        ArrayList<Character> alphabet = new ArrayList<>();
+        String input = br.readLine();
         for (char c = 'a'; c <= 'z'; c++) {
-            alphabet.add(c);
+            int idx = input.indexOf(c);
+            sb.append(idx+" ");
         }
-
-        String word = br.readLine();
-        int size = word.length();
-        HashSet<Character> visited = new HashSet<>();
-        for (int i = 0; i < 26; i++) {
-            char alpha = alphabet.get(i);
-            if (visited.contains(alpha)) {continue;}
-            boolean flag = false;
-
-            // alpha가 word안에 있는지 검사
-            int idx = 0;
-            for (int j = 0; j < size; j++) {
-                char s = word.charAt(j);
-                if (alpha == s) {
-                    flag = true;
-                    idx = j;
-                    break;
-                }
-            }
-            // flag = true라면 j값을 더하고 아니라면 -1값을 더함
-            if (flag) {
-                sb.append(idx + " ");
-            } else {
-                sb.append(-1 + " ");
-            }
-        }
-
         System.out.println(sb);
         br.close();
     }
