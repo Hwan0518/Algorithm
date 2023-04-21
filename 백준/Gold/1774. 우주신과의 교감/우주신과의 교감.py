a564ea53aab666c3abf503a598da130f8ba021ee
Hwan0518 <108791919+Hwan0518@users.noreply.github.com>
2023-04-21 19:53:28 +0900

    [Gold III] Title: 우주신과의 교감, Time: 2364 ms, Memory: 166268 KB -BaekjoonHub

--- 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.py
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.py
@@ -1,3 +1,31 @@
+'''
+Condition
+    - TL : 2s(4000만)
+      ML : 128mb
+    - n,m <=1000, x,y<=100만
+
+Question
+    - 
+
+Access
+    - 예상 분류 : 크루스칼, 프림
+      실제 분류 : 최소신장트리
+
+    try_1)
+        - prim 알고리즘을 사용해본다
+        >>> O(N^2logN) 가능
+    
+    try_2)
+        - 전체 신들을 탐색해야한다. 그리고 각각에서 최소연결값을 찾아야한다.
+        
+    try_3)
+        - 전체 node에서 최소를 이어본다
+        >>> 모든 노드가 연결됐는지 확인하는 방법을 모르겠음 ㅜㅜ
+        
+    try_4)
+        - kruskal을 사용해본다    
+    
+'''
 from math import sqrt
 from sys import stdin
 input = stdin.readline
@@ -14,6 +42,7 @@ def connect():
             cost = sqrt((cx-nx)**2 + (cy-ny)**2)
             graph.append((cost,i,j))
     graph.sort()
+    return
     
 
 def findParent(parent,node):
@@ -35,8 +64,7 @@ def unionParent(parent,stt,end):
 def kruskal():
     # 간선들을 전체탐색한다
     minCost = 0
-    for i in range(len(graph)):
-        cost,stt,end = graph[i]
+    for cost,stt,end in graph:
         sttParent = findParent(parent,stt)
         endParent = findParent(parent,end)
         # 부모가 다르다면 연결된 상태가 아니므로 연결시켜준다
