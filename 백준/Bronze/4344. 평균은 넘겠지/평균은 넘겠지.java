43c813fe3f1344026a8f1786271ca96df5f50839
Hwan0518 <108791919+Hwan0518@users.noreply.github.com>
2023-04-29 00:51:24 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 204 ms, Memory: 16712 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
@@ -13,8 +13,11 @@ Access
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
 
 
 public class Main {
@@ -28,21 +31,20 @@ public class Main {
         for (int i = 0; i < c; i++) {
             String input = br.readLine();
             // 성적 List : <학생 수 + 학생들 성적>
-            List<Integer> score = Arrays.stream(input.split(" "))
+            LinkedList<Integer> score = Arrays.stream(input.split(" "))
                     .map(Integer::parseInt)
-                    .collect(Collectors.toList());
+                    .collect(Collectors.toCollection(LinkedList::new));
             // 학생 수 = score의 첫 번째 값
             int size = score.get(0);
+            score.remove(0);
             // 총점 : score 모든 원소의 합 - score 첫 번째 값
             int total = score.stream()
-                    .skip(1)
                     .mapToInt(Integer::intValue)
                     .sum();
             // 평균 : 총점 / 학생 수
             double average = (double) total / (double) size;
             // 평균이 넘는 학생 수
             long count = score.stream()
-                    .skip(1)
                     .filter(x -> x > average)
                     .count();
             // 비율 : 평균이 넘는 학생 수 / 학생 수
