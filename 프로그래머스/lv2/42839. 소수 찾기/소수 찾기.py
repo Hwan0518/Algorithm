9946b5de6c8927f2c56e12e41ace753a7edead6d
Hwan0518 <108791919+Hwan0518@users.noreply.github.com>
2023-07-12 22:02:41 +0900

    [level 2] Title: 소수 찾기, Time: 45.82 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,48 @@
+'''
+Approach
+    - 길이가 1~7이하 문자열
+
+>>> 9999999 이하의 소수를 모두 찾아본다
+    
+
+'''
+import math
+
+
+def findPrime(limit):
+    allNum = [True]*(limit+1)
+    allNum[0] = allNum[1] = False
+    # 제곱근까지만 계산해서 소수인지 확인
+    for i in range(2, int(math.sqrt(limit)+1)):
+        # i의 배수를 모두 false처리
+        if allNum[i]:
+            j = 2
+            while i*j<=limit:
+                allNum[i*j] = False
+                j +=1
+    # flase가 아닌 수는 모두 프라임이다
+    return allNum
+
+
+def solution(numbers):
+    answer = 0
+    numbers = list(numbers)
+    limit = 10**len(numbers)
+    
+    # limit까지의 수 중에서 prime 수를 찾음
+    prime = findPrime(limit)
+    
+    # 소수 완탐
+    for i in range(2, limit):
+        flag = True
+        temp = numbers[:]
+        if prime[i]:
+            for s in str(i):
+                if s not in temp:
+                    flag = False
+                    break
+                temp.remove(s)
+            if flag:
+                answer +=1
+    
+    return answer
