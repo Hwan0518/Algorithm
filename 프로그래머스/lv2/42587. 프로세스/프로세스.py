ae4c788b60993749372890255db293440daa5ef2
Hwan0518 <108791919+Hwan0518@users.noreply.github.com>
2023-07-15 03:28:14 +0900

    [level 2] Title: 프로세스, Time: 0.06 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.py
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -8,33 +8,31 @@ Approach
 >>> deque 사용
     : 큐를 한번 돌려서 maxVal과 그 갯수를 파악해둔 후, location에 위치한 프로세스가 종료될때까지 실행한다
     
+>>> max를 사용하자
+    : n이 작기 때문에 그냥 max를 사용해도 될 것 같다
+    
 '''
 from collections import deque
 def solution(priorities, location):
     answer = 0
-    location +=1
+    l = location
     dq = deque(priorities)
     
     while dq:
         val = dq[0]
-        location -=1
-        # val == location일때
-        if location == 0:
-            # 우선순위가 최대일때
-            if val >= max(dq):
-                print(val)
-                answer +=1
-                return answer
-            # 우선순위가 최대가 아닐때
-            else:
-                dq.rotate(-1)
-                location += len(dq)
-                continue
-            
-        # val != location일때
-        if val >= max(dq):
-            answer +=1
+        # 목표가 우선순위일때
+        if l == 0 and val >= max(dq):
+            return answer +1
+        # 목표가 우선순위가 아닐때
+        elif l == 0:
+            dq.rotate(-1)
+            l += len(dq)
+        # 목표가 아니지만 우선순위일때
+        elif val >= max(dq):
             dq.popleft()
-            print(val)
+            answer +=1
+        # 목표도 아니고 우선순위도 아닐때
         else:
             dq.rotate(-1)
+        l -=1
+        
