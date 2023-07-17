d0d0be73a67c446bb39c32c2d35fcc2045241578
Hwan0518 <108791919+Hwan0518@users.noreply.github.com>
2023-07-18 01:23:06 +0900

    [Silver II] Title: 쇠막대기, Time: 72 ms, Memory: 31688 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -1,11 +1,13 @@
 '''
 Approach
     - () : 레이저
+    - n<=10만
     
 >>> 스택
     : 막대기 하나에 몇 개의 레이저가 들어가있는지 확인한다
       막대기 하나에서 갈라지는 수 = 들어있는 레이저 +1
       
+>>> 참고 : https://www.acmicpc.net/source/63646948
 
 '''
 
@@ -15,23 +17,21 @@ arr = input().strip()
 
 answer = 0
 stick = []
-if arr[0] == "(" : stick.append(0)
-for i in range(1,len(arr)):
-    before = arr[i-1]
+for i in range(len(arr)):
     cur = arr[i]
-    
-    if stick:
-        if before == cur == "(":
-            stick.append(0)
-        elif before == "(" and cur == ")":
+    # (면 막대기에 넣어준다
+    if cur == "(":
+        stick.append("(")
+    # )라면 이전걸 확인해서 처리한다
+    else:
+        before = arr[i-1]
+        # 레이저이므로 스틱을 하나 빼고, 갯수만큼 더해줌
+        if before == "(":
             stick.pop()
-            for i in range(len(stick)):
-                stick[i] += 1
-        elif before == ")" and cur == "(":
-            stick.append(0)
+            answer += len(stick)
+        # 스틱의 마지막 분리지점이므로 스틱을 하나 빼고, 1을 더해줌
         else:
-            answer += stick.pop()+1
+            stick.pop()
+            answer +=1
     
-    elif not stick and cur == "(":
-        stick.append(0)
 print(answer)
