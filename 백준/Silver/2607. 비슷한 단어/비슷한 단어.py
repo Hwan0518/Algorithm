fb6158fd470fa0cd6db460dda7325b97e53df58d
Hwan0518 <qip0518@naver.com>
2024-06-23 00:18:08 +0900

    [Silver II] Title: 비슷한 단어, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
@@ -1,12 +1,20 @@
-n = int(input())
-fst = list(input().strip())
-cnt = 0
-for _ in range(n-1):
-    f = fst[:]
+from sys import stdin
+input = stdin.readline
+
+def check(fst:list, word:str):
+    fst = fst[:]
     no = 0
-    for s in input().strip():
-        if s in f: f.remove(s)
+    for s in word:
+        if s in fst: fst.remove(s)
         else: no +=1
-    if len(f) <=1 and no <=1:
-        cnt +=1
-print(cnt)
+    return True if len(fst) <= 1 and no <=1 else False
+
+def main():
+    n = int(input())
+    fst = list(input().strip())
+    cnt = 0
+    for _ in range(n-1):
+        cnt += 1 if check(fst, input().strip()) else 0
+    return cnt
+
+print(main())
