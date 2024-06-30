b9de34e4b6b70a01e735471f45518c1eca2e3a97
Hwan0518 <qip0518@naver.com>
2024-07-01 00:57:26 +0900

    [Gold IV] Title: 최단경로, Time: 444 ms, Memory: 135356 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,52 +1,54 @@
 '''
-dijkstra
+Condition
+    - TL: 1s
+    - 
 
+Goals
+    - 시작점에서 다른 모든 정점으로의 최단 경로의 경로값을 출력
 
+Approach
+    - dijkstra
+    - TC = O((V+E)logV) = O(약 32만)
 '''
 from sys import stdin
 from heapq import *
 input = stdin.readline
 
-def inputData():
+def input_data():
     v,e = map(int,input().split())
-    stt = int(input())
-    graph =[[]*(v+1) for _ in range(v+1)]
+    s = int(input())
+    graph = [[] for _ in range(v+1)]
     for _ in range(e):
-        s,e,w = map(int,input().split())
-        graph[s].append((e,w))
-    return v,e,stt,graph
+        stt,end,cost = map(int,input().split())
+        graph[stt].append((end,cost))
+    return v,e,s,graph
 
-def solution(v,e,stt,graph):
+def dijkstra(s:int, graph:list[list]):
     global distance
-    Inf = int(1e9)
-    distance = [Inf] * (v+1)
-    
-    def dijkstra(stt, graph):
-        q = []
-        heappush(q, (0, stt))
-        distance[stt] = 0
-        while q:
-            # 가장 짧은 거리를 뽑아냄
-            dist, vertex = heappop(q)
-            # 기존값과 뽑아낸 값을 비교 -> 뽑아낸 값보다 기존값이 더 작다면 continue
-            if distance[vertex] < dist:
-                continue
-            # 뽑아낸 vertex와 연결된 인접 노드들을 탐색
-            for data in graph[vertex]:
-                neighbor = data[0]
-                cost = distance[vertex] + data[1]
-                # 더 작은 cost로 갱신된다면 q에 추가
-                if cost < distance[neighbor]:
-                    distance[neighbor] = cost
-                    heappush(q, (cost, neighbor))
-        return
-    dijkstra(stt, graph)
-    for c in distance[1:]:
-        if c == Inf:
-            print("INF")
-        else:
-            print(c)
+    hq = []
+    heappush(hq, (0, s))
+    while hq:
+        dist, node = heappop(hq)
+        if distance[node] < dist:
+            continue
+        # node의 인접 노드를 탐색
+        for data in graph[node]:
+            neighbor = data[0]
+            cost = distance[node] + data[1]
+            if cost < distance[neighbor]:
+                distance[neighbor] = cost
+                heappush(hq, (cost, neighbor))
     return
 
-solution(*inputData())
-    
+def main(v:int, e:int, s:int, graph:list[list]):
+    global distance
+    INF = int(1e9)
+    distance = [INF] * (v+1)
+    distance[s] = 0
+    dijkstra(s, graph)
+    # 출력
+    for i in range(1,v+1):
+        print(distance[i] if distance[i]!=INF else "INF")
+    return
+
+main(*input_data())
