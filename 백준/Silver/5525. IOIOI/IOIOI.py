f7d8b8501258f6b13970d51c9b379b459daff34f
Hwan0518 <qip0518@naver.com>
2024-07-09 20:33:50 +0900

    [Silver I] Title: IOIOI, Time: 144 ms, Memory: 115064 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -10,20 +10,27 @@ Goals
     - S에 Pn이 몇 개 존재하는가?
 
 Approach
-    - 투포인터
+    - 투포인터 (슬라이딩 윈도우) -> TLE (패턴과 현재문자열 비교하는 과정이 O(N)이라 불가능)
+    - flag를 가지고 다니면서 true인지 false인지 확인하자
 '''
-n = int(input())
-m = int(input())
+n,m = int(input()), int(input())
 s = input().strip()
+# IO 패턴
 pn = "IO"*n + "I"
-cur = ""
 cnt = 0
-r = 0
-for l in range(m):
-    while len(cur) < len(pn) and r < m:
-        cur += s[r]
-        r +=1
-    if cur == pn:
+p_cnt = 0
+# 탐색
+i=0
+while i < m:
+    cur = s[i:i+3]
+    if cur == "IOI":
         cnt +=1
-    cur = cur[1:]
-print(cnt)
+        i +=2
+        if cnt == n:
+            p_cnt +=1
+            cnt -=1
+    else:
+        cnt = 0
+        i +=1
+
+print(p_cnt)
