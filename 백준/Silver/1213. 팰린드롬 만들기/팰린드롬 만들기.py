039451e31b01619d3adabcf4e6e14429faafcdcb
Hwan0518 <qip0518@naver.com>
2024-11-26 20:27:41 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 56 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,24 +1,36 @@
 from collections import defaultdict
-sList = list(input().strip())
-sList.sort()
-sSet = set(tuple(sList))
-sDict = defaultdict(int)
-for s in sSet:
-    sDict[s] = sList.count(s)
-sDictKeys = sorted(sDict.keys())
-answer = []
-single = []
-for s in sDictKeys:
-    cnt = sDict[s]
-    if cnt%2:
-        answer.append(s*((cnt-1)//2))
-        single.append(s)
-    else:
-        answer.append(s*(cnt//2))
 
-if len(single)>1:
-    print("I'm Sorry Hansoo")
-else:
-    print(''.join(answer),end="")
-    print(single.pop(),end="") if single else print(end="")
-    print(''.join(answer[::-1]))
+def printAnswer(answer, single):
+    if len(single)>1:
+        print("I'm Sorry Hansoo")
+    else:
+        print(''.join(answer),end="")
+        print(single.pop(),end="") if single else print(end="")
+        print(''.join(answer[::-1]))
+
+def calc():
+    for s in sSet:
+        sDict[s] = sList.count(s)
+    sDictKeys = sorted(sDict.keys())
+    answer = []
+    single = []
+    for s in sDictKeys:
+        cnt = sDict[s]
+        if cnt%2:
+            answer.append(s*((cnt-1)//2))
+            single.append(s)
+        else:
+            answer.append(s*(cnt//2))
+    return answer, single
+
+def solution():
+    global sList, sSet, sDict
+    sList = sorted(list(input().strip()))
+    sSet = set(tuple(sList))
+    sDict = defaultdict(int)
+    # 계산
+    answer, single = calc()
+    # 정답 출력
+    printAnswer(answer, single)
+
+solution()
