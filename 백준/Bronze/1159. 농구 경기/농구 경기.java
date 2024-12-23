2d97da91d9849e77d8ba3821d9fbfede46078daf
Hwan0518 <qip0518@naver.com>
2024-12-23 23:17:38 +0900

    [Bronze II] Title: 농구 경기, Time: 100 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.java
+++ 백준/Bronze/1159. 농구 경기/농구 경기.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 
 /**
@@ -15,8 +12,7 @@ import java.util.Map;
  * - 5명보다 적다면 PREDAJA 출력
  *
  * approach
- * - HashMap 사용
- * - 입력받은 이름의 첫 번째 값-97의 cnt 증가
+ * - 입력받은 이름의 첫 번째 값-97('a')의 cnt 증가
  * - 모든 입력이 끝난 후 cnt가 5 이상인 값들을 sb에 추가 후 출력
  */
 
@@ -25,38 +21,21 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		Map<Character, Integer> result = new HashMap<>();
 		// 갯수 확인
+		char[] result = new char[26];
 		for (int i=0; i<n; i++) {
-			char first = br.readLine().toCharArray()[0];
-			Integer cnt = result.get(first);
-			if (cnt != null) {
-				result.put(first, cnt+1);
-			} else {
-				result.put(first, 1);
-			}
+			char first = br.readLine().charAt(0);
+			result[first-'a'] +=1;
 		}
 		// 5 이상인지 확인
-		StringBuilder sb = new StringBuilder();
-		for (Map.Entry<Character, Integer> entry: result.entrySet()) {
-			if (entry.getValue() >= 5) {
-				sb.append(entry.getKey());
+		StringBuilder answer = new StringBuilder();
+		for (int i=0; i<26; i++) {
+			if (result[i] >=5) {
+				answer.append(Character.toString(i + 'a'));
 			}
 		}
-		// 출력할 성이 있다면 정렬하여 출력
-		char[] array = sb.toString().toCharArray();
-		if (array.length == 0) {
-			System.out.println("PREDAJA");
-			return;
-		}
-		Arrays.sort(array);
-		StringBuilder sortedSb = new StringBuilder();
-		for (char c : array) {
-			sortedSb.append(c);
-		}
-		System.out.println(sortedSb);
+		// 출력
+		System.out.println(answer.length() == 0 ? "PREDAJA" : answer);
 	}
-
-
 
 }
