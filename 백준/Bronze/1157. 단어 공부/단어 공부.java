fed4d3867906487d1519bf47d8640bcf2dae15a6
Hwan0518 <qip0518@naver.com>
2024-12-28 17:25:36 +0900

    [Bronze I] Title: 단어 공부, Time: 240 ms, Memory: 22052 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -8,9 +8,8 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		char[] w = br.readLine().toCharArray();
 		int[] cnt = new int[26];
-		for (char a:w) {
+		for (char a:br.readLine().toCharArray()) {
 			if (a>=97) {
 				a -=97;
 			} else {
@@ -20,18 +19,16 @@ public class Main {
 		}
 		int maxV = 0;
 		char max = 0;
-		int maxCnt = 0;
 		for (int i=0; i<26; i++) {
 			int v = cnt[i];
 			if (v > maxV) {
 				max = (char) (i+65);
 				maxV = v;
-				maxCnt = 1;
 			} else if (v == maxV) {
-				maxCnt ++;
+				max = '?';
 			}
 		}
-		System.out.println(maxCnt==1 ? max : "?");
+		System.out.println(max);
 	}
 	
 }
