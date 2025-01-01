88feed8c6e72f27f1abd0bd6a232a36f03e847ae
Hwan0518 <qip0518@naver.com>
2025-01-01 09:37:22 +0900

    [Bronze III] Title: 골뱅이 찍기 - 정사각형, Time: 116 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/23794. 골뱅이 찍기 － 정사각형/골뱅이 찍기 － 정사각형.java
+++ 백준/Bronze/23794. 골뱅이 찍기 － 정사각형/골뱅이 찍기 － 정사각형.java
@@ -1,24 +1,20 @@
 import java.io.*;
 
 /**
- * 완탐 -> 60 * 60 * 24 = 86400, 최대 O(86400)으로 가능
+ * 
  */
 public class Main {	
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-		String upDown = "@" + "@".repeat(n) + "@"; 
-		String middle = "@" + " ".repeat(n) + "@" + "\n";
-		bw.append(upDown + "\n");
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine()); 
+		sb.append("@".repeat(n+2)).append("\n");
 		for (int i=0; i<n; i++) { 
-			bw.append(middle);
+			sb.append("@").append(" ".repeat(n)).append("@").append("\n");
 		}
-		bw.append(upDown);
-		bw.flush();
-		br.close();
-		bw.close();
+		sb.append("@".repeat(n+2));
+		System.out.print(sb);
 	}
 	
 }
