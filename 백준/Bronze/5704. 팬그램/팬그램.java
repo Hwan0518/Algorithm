0daa191dd87c651d8dfed378edb0957e791f2c5e
Hwan0518 <qip0518@naver.com>
2025-01-01 18:00:28 +0900

    [Bronze II] Title: 팬그램, Time: 116 ms, Memory: 14480 KB -BaekjoonHub

--- 백준/Bronze/5704. 팬그램/팬그램.java
+++ 백준/Bronze/5704. 팬그램/팬그램.java
@@ -8,21 +8,22 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		char[] input = br.readLine().toCharArray();
-		while (input[0] !='*') {
-			int[] cnt = new int[26];
+		String input = br.readLine();
+		while (!input.equals("*")) {
+			char[] arr = input.toCharArray();
+			boolean[] exist = new boolean[26];
 			boolean pangram = true;
-			for (char c:input) {
-				if (c>=97) cnt[c-97]++;
+			for (char c:arr) {
+				if (c>=97) exist[c-97] = true;
 			}
-			for (int count:cnt) {
-				if (count == 0) {
+			for (boolean e:exist) {
+				if (!e) {
 					pangram = false;
 					break;
 				}
 			}
 			sb.append(pangram ? "Y\n" : "N\n");
-			input = br.readLine().toCharArray();
+			input = br.readLine();
 		}
 		System.out.print(sb);
 		br.close();
