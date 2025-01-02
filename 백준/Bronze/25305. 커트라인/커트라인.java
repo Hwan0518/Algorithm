314fc362e415bc0c7b9604e4d9639ea4f234d9eb
Hwan0518 <qip0518@naver.com>
2025-01-02 10:24:08 +0900

    [Bronze II] Title: 커트라인, Time: 108 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.java
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -9,7 +9,8 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
 		int[] arr = new int[n];
 		for (int i=0; i<n; i++) {
