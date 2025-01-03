3f96c490087f03133c910274dd0b40dd022f81fd
Hwan0518 <qip0518@naver.com>
2025-01-03 16:33:35 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 880 ms, Memory: 252944 KB -BaekjoonHub

---
+++ 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Main {	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringBuffer sb = new StringBuffer();
+		for (int i=0; i<n; i++) {
+			int m = Integer.parseInt(br.readLine());
+			int min = Integer.MAX_VALUE; 
+			int max = -Integer.MAX_VALUE;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j=0; j<m; j++) {
+				int num = Integer.parseInt(st.nextToken());
+				min = Math.min(min, num);
+				max = Math.max(max, num);
+			}
+			sb.append(min +" "+max+"\n");
+		}
+		System.out.print(sb);
+	}
+	
+}
