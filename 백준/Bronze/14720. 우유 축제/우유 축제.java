49a2a9c55d6328ca825b27177253b97bbab6b9d7
Hwan0518 <qip0518@naver.com>
2025-01-03 16:59:34 +0900

    [Bronze II] Title: 우유 축제, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/14720. 우유 축제/우유 축제.java
+++ 백준/Bronze/14720. 우유 축제/우유 축제.java
@@ -4,41 +4,28 @@ import java.util.*;
 /**
  *딸기->초코->바나나->딸기
  */
-public class Main {	
+public class Main {
+	static int[] seq = {0,1,2,0};
+	static int cnt = 0;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int cur = 2;
-		int cnt = 0;
 		for (int i=0; i<n; i++) {
 			int next = Integer.parseInt(st.nextToken());
-			switch (cur) {
-			case 0 : {
-				if (next == 1) {
-					cur = 1;
-					cnt++; 
-				}
-				break;
-			}
-			case 1 : {
-				if (next == 2) {
-					cur = 2;
-					cnt++;
-				}
-				break;
-			}
-			case 2 : {
-				if (next == 0) {
-					cur = 0;
-					cnt++;
-				}
-				break;
-				}
-			}
+			cur = check(cur, next);
 		}
 		System.out.print(cnt);
+	}
+	
+	public static int check(int cur, int next) {
+		if (next == seq[cur+1]) {
+			cnt ++;
+			return seq[cur+1];
+		}
+		return cur;
 	}
 	
 }
