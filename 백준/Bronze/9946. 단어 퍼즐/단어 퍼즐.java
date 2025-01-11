27ac48ae13cc29b0e1cf69fb0de32ee2df8f7a82
Hwan0518 <qip0518@naver.com>
2025-01-12 01:29:44 +0900

    [Bronze I] Title: 단어 퍼즐, Time: 364 ms, Memory: 27364 KB -BaekjoonHub

--- 백준/Bronze/9946. 단어 퍼즐/단어 퍼즐.java
+++ 백준/Bronze/9946. 단어 퍼즐/단어 퍼즐.java
@@ -2,31 +2,37 @@ import java.io.*;
 import java.util.*;
 
 /**
- * hashmap 사용하자
+ * arr 사용하자
  */
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		Map<Character, Integer> m = new HashMap<>();
 		String w1 = br.readLine();
 		String w2 = br.readLine();
 		boolean isSame = true;
 		int cnt = 1;
 		while (!w1.equals("END") || !w2.equals("END")) {
-			char[] w11 = w1.toCharArray();
-			char[] w22 = w2.toCharArray();
-			for (int i=0; i<Math.max(w11.length, w22.length); i++) {
-				if (i < w11.length) m.put(w11[i], m.getOrDefault(w11[i], 0) +1);
-				if (i < w22.length) m.put(w22[i], m.getOrDefault(w22[i], 0) -1);
-			}
 			sb.append("Case ").append(cnt).append(": ");
-			for (int v:m.values()) {;
-				if (v != 0) {
-					sb.append("different\n");
-					isSame = false;
-					break;
+			int[] arr = new int[26];
+			// diff length
+			if (w1.length() != w2.length()) {
+				sb.append("different\n");
+				isSame = false;
+			} 
+			// same length
+			else {
+				for (int i=0; i<w1.length(); i++) {
+					arr[w1.charAt(i) - 'a'] ++;
+					arr[w2.charAt(i) - 'a'] --;
+				}
+				for (int v:arr) {
+					if (v != 0) {
+						sb.append("different\n");
+						isSame = false;
+						break;
+					}
 				}
 			}
 			sb.append(isSame ? "same\n" : "");
@@ -35,7 +41,6 @@ public class Main {
 			w2 = br.readLine();
 			isSame = true;
 			cnt++;
-			m.clear();
 		}
 		System.out.print(sb);
 	}
