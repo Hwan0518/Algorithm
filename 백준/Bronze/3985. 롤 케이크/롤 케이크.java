05a4113a3a9b5ce06edeaeb7a97de7e90f058cec
Hwan0518 <qip0518@naver.com>
2025-01-30 18:48:25 +0900

    [Bronze I] Title: 롤 케이크, Time: 136 ms, Memory: 14952 KB -BaekjoonHub

---
+++ 백준/Bronze/3985. 롤 케이크/롤 케이크.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * L 길이의 케이크
+ * N명의 방청객
+ * P부터 K까지를 원함
+ * 이미 선점되면 주지 못하고 넘어감
+ * 가장 많은 조각을 받을 것으로 기대한 방청객 번호와, 실제로 가장 많이 받은 방청객 번호는?
+ * => 차례대로 완탐 돌리면 될 것 같음
+ */
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		// input
+		int l = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		int[][] wantList = new int[n][2];
+		// calc expected
+		int expectedMaxVal = 0;
+		int expectedMaxVisit = 0;
+		for (int i=0; i<n; i++) {
+			wantList[i] = Arrays.stream(br.readLine().split(" "))
+					.mapToInt(Integer::parseInt)
+					.toArray();
+			int len = wantList[i][1]-wantList[i][0]+1; 
+			if (len > expectedMaxVal) {
+				expectedMaxVal = len;
+				expectedMaxVisit = i;
+			}
+		}
+		// calc real
+		int[] selected = new int[l+1];
+		Arrays.fill(selected, -1);
+		int[] visitor = new int[n];
+		int realMaxVal = 0;
+		int realMaxVisit = 0;
+		for (int i=0; i<n; i++) {
+			int[] want = wantList[i];
+			for (int j=want[0]; j<=want[1]; j++) {
+				if (selected[j] == -1) {
+					selected[j] = i;
+					visitor[i] ++;
+				}
+			}
+			if (visitor[i] > realMaxVal) {
+				realMaxVal = visitor[i];
+				realMaxVisit = i;
+			}
+		}
+		// answer
+		sb.append(expectedMaxVisit+1).append("\n").append(realMaxVisit+1);
+		System.out.print(sb);
+	}
+		
+}
