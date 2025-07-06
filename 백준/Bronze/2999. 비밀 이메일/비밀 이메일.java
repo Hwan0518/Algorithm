60a20c6170b954c30d47bf894e40a95929b07d5d
Hwan0518 <qip0518@naver.com>
2025-07-07 00:40:19 +0900

    [Bronze I] Title: 비밀 이메일, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/2999. 비밀 이메일/비밀 이메일.java
+++ 백준/Bronze/2999. 비밀 이메일/비밀 이메일.java
@@ -12,30 +12,23 @@ public class Main {
 		
 		// calc r,c
 		int n = input.length();
+		int limit = (int) Math.sqrt(n);
 		int r = 1;
 		int c = n;
-		int temp = 1;
-		while (temp < c) {
-			if (n%temp == 0) {
-				if (temp <= n/temp) {
-					r = temp;
-					c = n/temp;
-				} else {
-					temp --;
-					break;
-				}
+		for (int temp = limit; temp>0; temp--) {
+			if (n%temp ==0) {
+				r = temp;
+				c = n/temp;
+				break;
 			}
-			temp ++;
 		}
 		
 		// get result
 		StringBuffer sb = new StringBuffer();
-		int idx = 0;
 		for (int i=0; i<r; i++) {
 			for (int j=0; j<c; j++) {
-				sb.append(input.charAt(idx+r*j));
+				sb.append(input.charAt(i + r*j));
 			}
-			idx++;
 		}
 		System.out.println(sb);
 		
