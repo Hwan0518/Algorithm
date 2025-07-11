c7dc41b6f9ca739da582a6d9dbf987921a9791ef
Hwan0518 <qip0518@naver.com>
2025-07-11 17:45:32 +0900

    [Bronze I] Title: Lifeguards (Bronze), Time: 100 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/15593. Lifeguards （Bronze）/Lifeguards （Bronze）.java
+++ 백준/Bronze/15593. Lifeguards （Bronze）/Lifeguards （Bronze）.java
@@ -16,6 +16,9 @@ import java.util.*;
  * 		-> 매번 99개 더하기(O(99)) * 100번반복(O(100)) = 9900
  * 		-> 완탐가능
  * 	- 입력 저장해두고, 차례대로 제외하면서 모두 더해서 max time을 구해보자
+ * 	- 두 번째 풀이 (feat. lms)
+ * 		-> 처음에 모든 covers를 구한 후, 하나씩 빼주면 됨
+ * 		-> int[]로 체크하는 방법과 set 사용하는 방법이 있음. 어떤거할까
  */
 
 public class Main {
@@ -25,26 +28,42 @@ public class Main {
 		// input
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
+		
+		// sum all times
 		int[][] lifeguards = new int[n][2];
+		int[] covers = new int[1000];
+		int allTimes = 0;
 		for (int i=0; i<n; i++) {
+			// input
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			lifeguards[i][0] = Integer.parseInt(st.nextToken());
-			lifeguards[i][1] = Integer.parseInt(st.nextToken());
+			int stt = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			// set
+			lifeguards[i][0] = stt;
+			lifeguards[i][1] = end;
+			// add times
+			for (int t=stt; t<end; t++) {
+				if (covers[t] == 0) {
+					allTimes ++;
+				}
+				covers[t] ++;
+			}
 		}
 		
 		// find max-time
-		int maxTime = 0;
+		int maxTime=0;
 		for (int i=0; i<n; i++) {
-			Set<Integer> covers = new HashSet<>();
-			for (int j=0; j<n; j++) {
-				// except 'i'st life guard
-				if (i==j) continue;
-				for (int time=lifeguards[j][0]; time<lifeguards[j][1]; time++) {
-					covers.add(time);
+			int stt = lifeguards[i][0];
+			int end = lifeguards[i][1];
+			// calc
+			int exceptTime = 0;
+			for (int t=stt; t<end; t++) {
+				if (covers[t] == 1) {
+					exceptTime++;
 				}
 			}
-			// update max-time
-			maxTime = Math.max(maxTime, covers.size());
+			// update
+			maxTime = Math.max(maxTime, allTimes-exceptTime);
 		}
 		
 		// result
