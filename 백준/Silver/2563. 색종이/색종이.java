ae690c017a7ec4f7e9a0987cd0be29da95762bdf
Hwan0518 <qip0518@naver.com>
2025-07-14 13:22:01 +0900

    [Silver V] Title: 색종이, Time: 160 ms, Memory: 17884 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,33 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+        
+		// input
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		// find total area
+		Set<String> area = new HashSet<>();
+		for (int i=0; i<n; i++) {
+			// input r,c
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			// push confetti loc to set
+			for (int x=r; x<r+10; x++) {
+				for (int y=c; y<c+10; y++) {
+					area.add(String.valueOf(x)+","+String.valueOf(y));
+				}
+			}
+		}
+		
+		// result
+		System.out.print(area.size());
+		
+    }
+	
+}
