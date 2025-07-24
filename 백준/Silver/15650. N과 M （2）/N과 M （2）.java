b168737fde6656f5435a6d009315df301d8962b4
Hwan0518 <qip0518@naver.com>
2025-07-24 17:21:36 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,55 @@
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	
+	static int n;
+	static int m;
+	static boolean[] visited;
+	static int[] selected;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws IOException {
+        
+		// input
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		// Backtracking
+		visited = new boolean[n+1];
+		selected = new int[n+1];
+		sb = new StringBuilder();
+		dfs(0);
+		
+		// result
+		sb.setLength(sb.length()-1);
+		System.out.print(sb);
+		
+	}
+	
+	
+	// backtracking dfs
+	public static void dfs(int cnt) {
+		if (cnt == m) {
+			for (int i=1; i<=m; i++) {
+				sb.append(selected[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i=1; i<=n; i++) {
+			if (visited[i] || i < selected[cnt]) {
+				continue;
+			}
+			visited[i] = true;
+			selected[cnt+1] = i;
+			dfs(cnt+1);
+			visited[i] = false;
+		}
+	}
+		
+}
