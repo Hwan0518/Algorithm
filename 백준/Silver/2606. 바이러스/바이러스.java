8439dfb111a2a1173bec97b7beb058a2c28cdc8d
Hwan0518 <qip0518@naver.com>
2025-08-28 20:22:06 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,63 @@
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+	static StringTokenizer st;
+	static int n;
+	static int m;
+	static List<Integer>[] edge;
+	static boolean[] visited;
+	static int cnt = 0;
+	
+	public static void main(String[] args) throws IOException {
+
+		// input
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
+		
+		edge = new ArrayList[n+1];
+		for (int i=1; i<n+1; i++) edge[i] = new ArrayList<>();
+		
+		for (int i=0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			edge[a].add(b);
+			edge[b].add(a);
+		}
+		
+		
+		// dfs
+		visited = new boolean[n+1];
+		visited[1] = true;
+		dfs(1);
+		
+		
+		// result
+		System.out.print(cnt);
+			
+	}
+	
+	
+	
+	static void dfs(int node) {
+		
+		for (int next : edge[node]) {
+		
+			if (visited[next]) continue;
+			visited[next] = true;
+			cnt ++;
+			
+			dfs(next);
+		
+		}
+	}
+		
+	
+	
+}
