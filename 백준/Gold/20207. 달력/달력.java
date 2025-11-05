dff97aaf1e3fa0fb2357f5dc509385a4f5abb496
Hwan0518 <qip0518@naver.com>
2025-11-05 21:56:33 +0900

    [Gold V] Title: 달력, Time: 128 ms, Memory: 16420 KB -BaekjoonHub

--- 백준/Gold/20207. 달력/달력.java
+++ 백준/Gold/20207. 달력/달력.java
@@ -28,7 +28,7 @@ public class Main {
 	static int findArea() throws IOException {
 
 		// sort schedules
-		int[][] schedules = new int[n][2];
+		int[] count = new int[365+1];
 
 		for (int i=0; i<n; i++) {
 
@@ -36,57 +36,31 @@ public class Main {
 			int stt = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
 
-			schedules[i][0] = stt;
-			schedules[i][1] = end;
+			for (int j=stt; j<=end; j++) count[j] ++;
 		}
-
-		Arrays.sort(schedules, (o1, o2) -> {
-			return o1[0] != o2[0]
-				? o1[0] - o2[0]  // stt 오름차순
-				: o2[1] - o1[1]; // end 내림차순
-		});
 
 		// calc Area
 		int area = 0;
-		boolean[][] visited = new boolean[n+1][365+1];
 
-		int maxR = 1;
-		int maxC = schedules[0][1];
-		int minC = schedules[0][0];
-		int lastDay = schedules[0][1];
+		int x = 0;
+		int y = 0;
+		for (int day=1; day<=365; day++) {
 
-		for (int i=0; i<n; i++) {
-
-			int curStt = schedules[i][0];
-			int curEnd = schedules[i][1];
-
-			// 현재 일정을 입력할 row를 계산
-			int curR = 1;
-			for (int c=curStt; c<=curEnd; c++) {
-				while (visited[curR][c]) curR++;
+			// 끊어졌다면 면적 계산 & x,y 갱신
+			if (count[day] == 0) {
+				area += x * y;
+				x = 0;
+				y = 0;
 			}
 
-			// 방문처리
-			for (int c = curStt; c <= curEnd; c++) visited[curR][c] = true;
-
-			// 연속되는 경우
-			if (curStt <= lastDay+1) {
-				maxR = Integer.max(maxR, curR); // maxR은 curR과 비교해서 갱신
-				maxC = Integer.max(maxC, curEnd); // maxC는 curEnd와 비교해서 갱신
-			}
-
-			// 연속되지 않음
+			// 연속됐다면 x와 y갱신
 			else {
-				area += maxR * (maxC-minC+1);
-				maxR = 1;
-				minC = curStt;
-				maxC = curEnd;
+				x = Integer.max(x, count[day]);
+				y ++;
 			}
-
-			lastDay = Integer.max(lastDay, curEnd);
 		}
 
-		if (schedules[n-1][0] <= lastDay+1) area += maxR * (maxC-minC+1);
+		if (count[365] != 0) area += x*y;
 
 		return area;
 	}
