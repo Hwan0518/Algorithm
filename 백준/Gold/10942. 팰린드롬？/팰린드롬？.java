0c880b4262d4d8575841bde91717d9f4e597fcbd
Hwan0518 <qip0518@naver.com>
2025-11-27 23:58:32 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1164 ms, Memory: 231628 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+
+
+public class Main {
+
+	static BufferedReader br;
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+
+	static int[][] dp;
+	static int n;
+	static int[] arr;
+
+
+
+	public static void main(String[] args) throws IOException {
+
+		// input
+		br = new BufferedReader(new InputStreamReader(System.in));
+
+		n = Integer.parseInt(br.readLine());
+
+		st = new StringTokenizer(br.readLine());
+		arr = new int[n+1];
+		for (int i=1; i<=n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+		dp = new int[n+1][n+1];
+		for (int i=1; i<=n; i++) {
+			for (int j=1; j<=n; j++) dp[i][j] = -1;
+		}
+
+		// question
+		int q = Integer.parseInt(br.readLine());
+		for (int i=0; i<q; i++) {
+
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+
+			int ans = dfs(s, e);
+			sb.append(ans).append("\n");
+		}
+
+		// result
+		System.out.print(sb);
+	}
+
+
+	static int dfs(int s, int e) {
+
+		// memoization
+		if (dp[s][e] == -1) {
+
+			// check palindrome
+			boolean valid = true;
+
+			for (int i=0; i<(e-s+1)/2; i++) {
+				if (arr[s+i] != arr[e-i]) {
+					valid = false;
+					break;
+				}
+			}
+
+			dp[s][e] = valid ? 1 : 0;
+		}
+
+		// res
+		return dp[s][e];
+	}
+
+}
+
