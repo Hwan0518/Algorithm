126990c22671b41b0ad8095b3a686c27fa7b29cc
Hwan0518 <qip0518@naver.com>
2025-12-10 23:18:02 +0900

    [Silver V] Title: 생일, Time: 136 ms, Memory: 16152 KB -BaekjoonHub

---
+++ 백준/Silver/5635. 생일/생일.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.time.LocalDate;
+import java.util.*;
+
+public class Main {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+
+	public static void main(String[] args) throws IOException {
+
+		// input
+		int n = Integer.parseInt(br.readLine());
+
+		// compare
+		LocalDate youngestBirth = LocalDate.of(1, 1, 1);
+		LocalDate oldestBirth = LocalDate.of(9999, 12, 31);
+		String youngest = "";
+		String oldest = "";
+
+		for (int i=0; i<n; i++) {
+
+			st = new StringTokenizer(br.readLine());
+
+			String name = st.nextToken();
+			int day = Integer.parseInt(st.nextToken());
+			int month = Integer.parseInt(st.nextToken());
+			int year = Integer.parseInt(st.nextToken());
+
+			LocalDate curBirth = LocalDate.of(year, month, day);
+
+			// youngest
+			if (curBirth.isAfter(youngestBirth)) {
+				youngestBirth = curBirth;
+				youngest = name;
+			}
+
+			else if (curBirth.isBefore(oldestBirth)) {
+				oldestBirth = curBirth;
+				oldest = name;
+			}
+		}
+		
+		// result
+		System.out.print(youngest + "\n" + oldest);
+	}
+
+}
