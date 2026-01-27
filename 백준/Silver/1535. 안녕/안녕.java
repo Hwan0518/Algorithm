3d7d648dab505e7fc4895a4d467cc2d1fbca1a56
Hwan0518 <qip0518@naver.com>
2026-01-28 01:35:44 +0900

    [Silver II] Title: 안녕, Time: 100 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.java
+++ 백준/Silver/1535. 안녕/안녕.java
@@ -14,6 +14,7 @@ public class Main {
 
 	static int n;
 	static int[] l,j;
+	static int[][] dp;
 
 	public static void main(String[] args) throws IOException {
 
@@ -28,8 +29,47 @@ public class Main {
 		j = new int[n];
 		for (int i=0; i<n; i++) j[i] = Integer.parseInt(st.nextToken());
 
-		int max = dfs(0, 100);
+		dp = new int[n][1 + 100];
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<=100; j++) dp[i][j] = -1;
+		}
+
+		// dfs
+//		int max = dfs(0, 100);
+		int max = dfs2(0, 100);
+
+		// result
 		System.out.print(max);
+	}
+
+
+	// hp에서 i를 골랐을때의 최대 행복을 리턴
+	static int dfs2(int i, int hp) {
+
+		// base-condition
+		if (hp <= 0 || i == n) {
+			if (hp <= 0) return -1_000_000;
+			return 0;
+		}
+
+		// memoization
+		if (dp[i][hp] == -1) {
+
+			// find max happy
+			int happy = 0;
+
+			// select
+			happy = Integer.max(happy, dfs2(i+1, hp-l[i]) + j[i]);
+
+			// non-select
+			happy = Integer.max(happy, dfs2(i+1, hp));
+
+			// assign max
+			dp[i][hp] = happy;
+		}
+
+		// result
+		return dp[i][hp];
 	}
 
 
